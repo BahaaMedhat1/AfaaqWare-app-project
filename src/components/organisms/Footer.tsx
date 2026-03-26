cd0c39250aad03e89bf541218b096f0b5bf7eb67
eslam-mohamedl <ee.eslam355@gmail.com>
2026-03-26 21:49:15 +0200

    add home page

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,8 +1,7 @@
-
-// export default function Footer() {
-//   return (
-//     <div className="bg-amber-500 w-full flex justify-center items-center p-10 text-white text-4xl">
-//       Footer
-//     </div>
-//   );
-// }
+export default function Footer() {
+  return (
+    <div className="bg-gray-500 w-full flex justify-center items-center p-10 text-white text-4xl">
+      Footer
+    </div>
+  );
+}
