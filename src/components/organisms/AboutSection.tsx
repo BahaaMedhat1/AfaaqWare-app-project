f25470a2e06a4eea99ce278a16d191081ff9a0ee
mohamednkklk534 <elzerossss@gmail.com>
2026-03-28 21:16:28 +0200

    add style for About Section

--- src/components/organisms/AboutSection.tsx
+++ src/components/organisms/AboutSection.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const AboutSection = () => {
   return (
-    <div>AboutSection</div>
-  )
+<div className="bg-gray-400 text-white w-full p-5 flex justify-center items-center">
+      AboutSection
+    </div>  )
 }
 
 export default AboutSection
