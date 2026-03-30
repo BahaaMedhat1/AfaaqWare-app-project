0c27f97d6339e8be98df9e82573fd385540df4dc
Amal <amal.nasr.1421@gmail.com>
2026-03-30 15:14:08 +0200

    Add Hero Section

---
+++ src/Organisms/HeroSection.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function HeroSection() {
+  return (
+    <div className='text-white font-bold text-2xl text-center bg-blue-700'>HeroSection</div>
+  )
+}
