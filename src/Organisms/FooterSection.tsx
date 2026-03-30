12559dfd19233dbbf0f7888b4e4f87c511b1b3f2
BahaaMedhat1 <bahaawanas427@gmail.com>
2026-03-30 19:22:16 +0200

    Adding Footer Section Feature

---
+++ src/Organisms/FooterSection.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function FooterSection() {
+  return (
+    <div className="text-white font-bold text-2xl text-center bg-blue-400  mb-6 py-10">
+      FooterSection
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeaderSection from "@/Organisms/HeaderSection";
 import MainSection from "@/Organisms/MainSection";
+import FooterSection from "@/Organisms/FooterSection";
 
 export default function page() {
   return (
     <div>
       <HeaderSection />
       <MainSection />
+      <FooterSection />
     </div>
   );
 }
